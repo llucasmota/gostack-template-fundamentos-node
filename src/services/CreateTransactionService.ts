65e482355afbd354de65705e40553d88097c1fc4
Lucas Mota <lucas.o.mota@gmail.com>
2020-05-19 21:03:33 -0300

    Retirando tratamentos da classe Repository e deixando o tratamento da criação da transação com o serviço responsável pelo mesmo

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,17 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: CreateTransactionDTO): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Tipo não permitido.');
+    }
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (value > total) {
+        throw Error('Outcome não pode ser maior que income');
+      }
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       type,
